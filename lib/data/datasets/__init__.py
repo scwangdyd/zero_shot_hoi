9ba4615342b9c5c7bbec653c11b136b588569074
SteveWangDYD <scwangntu@gmail.com>
2020-05-10 23:18:12 +0800

    vcoco init

--- lib/data/datasets/__init__.py
+++ lib/data/datasets/__init__.py
@@ -1,5 +1,6 @@
 from . import builtin  # ensure the builtin datasets are registered
 from .hico import load_hico_json
 from .hico_meta import HICO_OBJECTS
+from .vcoco_meta import VCOCO_OBJECTS
 
 __all__ = [k for k in globals().keys() if "builtin" not in k and not k.startswith("_")]
